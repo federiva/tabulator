1a1d77aa75b870f6951d3bb9aafda3c80a2c4a0b
federiva <federivadeneira@gmail.com>
2023-10-01 21:00:38 -0300

    feat: Using tabulator

---
+++ R/tabulator.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+tabulator <- function(data, width = NULL, height = NULL, elementId = NULL) {
+  data <- check_for_valid_column_names(data)
+  # forward options using x
+  params = list(
+    data = data
+  )
+  attr(params, 'TOJSON_ARGS') <- list(dataframe = "rows")
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'tabulator',
+    x = params,
+    width = width,
+    height = height,
+    package = 'tabulator',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for tabulator
+#'
+#' Output and render functions for using tabulator within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a tabulator
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name tabulator-shiny
+#'
+#' @export
+tabulatorOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'tabulator', width, height, package = 'tabulator')
+}
+
+#' @rdname tabulator-shiny
+#' @export
+renderTabulator <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, tabulatorOutput, env, quoted = TRUE)
+}
