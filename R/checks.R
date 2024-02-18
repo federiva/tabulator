0554192f4f3e02330695af6937a82829b98457c7
federiva <federivadeneira@gmail.com>
2024-02-18 18:13:39 -0300

    docs: Documenting

--- R/checks.R
+++ R/checks.R
@@ -1,8 +1,14 @@
+#' Run checks for the tabulator object that is being built by the user
+#' @noRd
 run_checks <- function(tabulator_object) {
   tabulator_object |>
     check_for_valid_nested_separator()
 }
 
+#' Checks for valid nested field separator. Checks that the column names of the
+#' data frame does not contain the same characters as the nestedFieldSeparator
+#' either specified by the user or the one used as default.
+#' @noRd
 check_for_valid_nested_separator <- function(tabulator_object) {
   data <- tabulator_object$x$data
   nested_field_sep <- tabulator_object$x$table_options$nestedFieldSeparator
